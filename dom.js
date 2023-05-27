ecd5d2b2baf84cde3eece66a7c34e51c3c30ec40
Abdulajishs <abdulajeeshsb@gmail.com>
2023-05-28 00:44:26 +0530

    getElementsByTagName

--- dom.js
+++ dom.js
@@ -35,22 +35,43 @@
 
 
 
-//GETELEMENTBYCLASSNAME
+// //GETELEMENTBYCLASSNAME
 
 // console.log(document.getElementsByClassName("list-group-item"));
-var items = document.getElementsByClassName("list-group-item");
+// var items = document.getElementsByClassName("list-group-item");
 // console.log(items);
-console.log(items[1]);
-items[1].textContent = "HELLO 2";
-items[2].style.backgroundColor = "green"
+// console.log(items[1]);
+// items[1].textContent = "HELLO 2";
+// items[2].style.backgroundColor = "green"
+
+// //Gives error
+// // items.style.backgroundColor = "yellow" //it's not working
+
+// // for (let i = 0; i < items.length; i++) {
+// //     items[i].style.backgroundColor = "yellow"
+// // }
+
+// for (let i = 0; i < items.length; i++) {
+//         items[i].style.fontWeight = "bold";
+//     }
+
+
+//GETELEMENTBYTAGNAME
+
+console.log(document.getElementsByTagName("li"));
+var li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "HELLO 2";
+li[2].style.backgroundColor = "green"
 
 //Gives error
 // items.style.backgroundColor = "yellow" //it's not working
 
-// for (let i = 0; i < items.length; i++) {
-//     items[i].style.backgroundColor = "yellow"
-// }
+for (let i = 0; i < li.length; i++) {
+    li[i].style.backgroundColor = "yellow"
+}
 
-for (let i = 0; i < items.length; i++) {
-        items[i].style.fontWeight = "bold";
-    }
+// for (let i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = "bold";
+//     }
